424655c19db4c88fc888e37fb310482d681a6552
Artyom Overchuk <ktulu1317@gmail.com>
2025-12-06 01:48:46 +0600

    [ObjectPoolTests] WhenPoolIsEmpty_ThenGetReturnsNull

---
+++ src/ultimate-tic-tac-toe/Assets/Scripts/Tests/EditMode/ObjectPoolTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Runtime.Services.UI;
+
+namespace Tests.EditMode
+{
+    [TestFixture]
+    public class ObjectPoolTests
+    {
+        private ObjectPool<object> _pool;
+
+        [SetUp]
+        public void SetUp() => _pool = new ObjectPool<object>();
+
+        #region Get Tests
+
+        [Test]
+        public void WhenPoolIsEmpty_ThenGetReturnsNull()
+        {
+            // Arrange
+            // Pool is empty by default
+
+            // Act
+            var result = _pool.Get<TestClass>();
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        #endregion
+
+        #region Test Classes
+
+        private class TestClass { }
+
+        #endregion
+    }
+}
+
